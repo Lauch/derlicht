58511b23cf9ec4a5779120b5483567883250f86d
Anonymooseable <anonymouse2048@gmail.com>
2012-05-17 15:28:00 +0200

    REALLY fixed it. I think.

--- derlicht.py
+++ derlicht.py
@@ -10,28 +10,30 @@ class MainWindow(wx.Frame):
 
 	def create_components(self):
 		#self.scenes_panel = wx.Panel(self,-1, style=wx.SUNKEN_BORDER)
-		
-		self.scroll = wx.ScrolledWindow( self, -1 )
-		self.panel = wx.Panel( self, -1 )
-		
-		self.sizer_main = wx.BoxSizer(wx.VERTICAL)
-		self.sizer_sliders = wx.BoxSizer(wx.HORIZONTAL)
+		self.main_panel = wx.Panel( self, -1 )
+		self.main_sizer = wx.BoxSizer(wx.VERTICAL)
+
+		self.scroll = wx.ScrolledWindow( self.main_panel, -1 )
+		self.scroll.SetScrollbars(1, 1, 1, 1)
+		self.sliders_sizer = wx.BoxSizer(wx.HORIZONTAL)
 		
 		self.sliders = []
 		for i in range(128):
-			s = wx.Slider(self.panel,-1, maxValue=255,style=wx.SL_VERTICAL)
+			s = wx.Slider(self.scroll,-1, maxValue=255,style=wx.SL_VERTICAL)
 			self.sliders.append(s)
-			self.sizer_sliders.Add(s, 1, wx.EXPAND)
+			self.sliders_sizer.Add(s, 1, wx.EXPAND)
 		
-		self.sizer_main.Add(self.sizer_sliders, proportion=1, flag=wx.ALL|wx.EXPAND)
-		
-		self.panel.SetSizer(self.sizer_main)
-		self.panel.SetAutoLayout( True )
-		self.panel.Layout()
-		self.panel.Fit()
-		
-		self.SetSizer(self.sizer_main)
-		self.SetAutoLayout(1)
+		self.main_sizer.Add(self.scroll, proportion=1, flag=wx.ALL|wx.EXPAND)
+
+		self.main_panel.SetSizer(self.main_sizer)
+		self.main_panel.SetAutoLayout( True )
+		self.main_panel.Layout()
+		self.main_panel.Fit()
+
+		self.scroll.SetSizer(self.sliders_sizer)
+		self.scroll.SetAutoLayout(True)
+		self.scroll.Layout()
+		self.scroll.Fit()
 		
 		self.Bind(wx.EVT_CLOSE, self.OnQuit)
 	def OnQuit(self, *args):
