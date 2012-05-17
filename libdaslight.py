a4999026d2799b5d682ce4d2604e0c61d954a9cb
Marius Schiffer <marius.schiffer@gmail.com>
2012-05-17 12:24:55 +0200

    Renamed daslight.py to derlicht.py (Project name) Moved USB stuff into separate thread class.

--- libdaslight.py
+++ libdaslight.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 import usb.core, usb.util
 import time
-class DasLightVC(object):
+import threading
+
+class DasLightInterface(threading.Thread):
+	"""
+	Handles all the interfacing with the hardware, including the USB interface.
+	"""
 	def __init__(self):
-		pass
+		self.values = [0] * 128
+		self.values_lock = threading.Lock()
+		self.setup_device()
 		
 	def _find_device(self):
 		"""
@@ -12,6 +19,10 @@ class DasLightVC(object):
 		self.dev = usb.core.find(idVendor=0x6244)
 	
 	def setup_device(self):
+		"""
+		Sets up the device, configuration, interface and endpoint.
+		We could also directly use device.write()
+		"""
 		self._find_device()
 		self.dev.set_configuration()
 		self.cfg = self.dev.get_active_configuration()
@@ -34,17 +45,56 @@ class DasLightVC(object):
 			except Exception as e2:
 				print "Fatal error"
 				print e
-				print "Terminating..."
 				return
 			else:
 				time.sleep(0.5)
-				self.write_to_device(s)
+				self.write_to_device(s)	
+	
+	def run(self):
+		while True:
+			self.values_lock.acquire(True) # blocking
+
+			# validate, move to set_values
+			if len(values) < 128:
+				values = values+[0 for i in range(128-len(values))]
+			assert len(values) == 128
+			
+			dmx_str = "".join(map(chr, values))
+			self.write_to_device(dmx_str)
+			self.values_lock.release()
+			time.sleep(0.04)
+	
+class DasLightVC(object):
+	"""
+	Manages the thread and stuff.
+	"""
+	
+	
+	def __init__(self):
+		self._thread = DasLightInterface()
+		self._thread.start()
+	
+	def get_values(self):
+		# This is probably not necessary, rather store it in this class
+		# The thread doesn't change values
+		self._thread.values_lock.acquire(True)
+		ret = self._thread.values
+		self._thread.values_lock.release()
 		
+	def set_values(self, vals):
+		# TODO implement validating here
 		
+		self._thread.values_lock.acquire(True)
+		self._thread.values = vals
+		self._thread.values_lock.release()
+	
+	values = property(get_values, set_values)
+	
 	def send_dmx(self, values):
-		if len(values) < 512:
-			values = values+[0 for i in range(512-len(values))]
-		assert len(values) == 512
+		"""
+		Takes an array and sends
+		"""
+		
 		dmx_str = "".join(map(chr, values))
 		print values
 		self.write_to_device(dmx_str)
@@ -52,8 +102,6 @@ class DasLightVC(object):
 		
 if __name__ == "__main__":
 	dl = DasLightVC()
-	dl.setup_device()
-	while True:
-		dl.send_dmx([65,88,10,33])
-		time.sleep(0.04)
+	dl.values = [1,2,3]
+	raw_input()
 		
